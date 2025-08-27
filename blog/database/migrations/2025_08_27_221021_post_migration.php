98a0946a9911f4e8abba35837fb929fbcef1ad69
itspamelie <pamelamartinez2003@hotmail.com>
2025-08-27 16:26:04 -0600

    modificacion a la base de datos
    Interesante

---
+++ blog/database/migrations/2025_08_27_221021_post_migration.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /*Dar de alta los campos de la tabla*/
+        Schema::create('posts', function(Blueprint $table){
+            $table->$id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('img',100);
+            $table->string('content',100);
+            $table->string('likes',100);
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -5,7 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/contacto',function(){
-        return view('contacto');
+Route::get('/contact',function(){
+        return view('contact');
 
+});
+Route::get('/about',function(){
+        return view('about');
+
+});
+Route::get('/post',function(){
+        return view('post');
 });
