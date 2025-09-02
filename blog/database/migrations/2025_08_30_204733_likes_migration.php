d7a3e902d38fbe397e4126366a74e08de7374678
itspamelie <pamelamartinez2003@hotmail.com>
2025-09-02 15:05:43 -0600

    Correccion
    Correccion de llaves foraneas

--- blog/database/migrations/2025_08_30_204733_likes_migration.php
+++ blog/database/migrations/2025_08_30_204733_likes_migration.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
          Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->foreignId('user_id')->constrained('users');
             $table->integer('post_id');
         });
     }
